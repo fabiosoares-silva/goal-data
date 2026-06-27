a00659596e84d28fca7655fc519b09f91bce368c
fabiosoares-silva <fabiosores.soares@gmail.com>
2026-06-27 10:20:23 -0300

    feat: add CompetitionRepository scaffold to Infrastructure

---
+++ GoalData.Application/Interfaces/ICompetitionRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoalData.Domain.Models;
+
+namespace GoalData.Application.Interfaces;
+
+public interface ICompetitionRepository
+{
+    Task<Competition> GetByCodeAsync(string code);
+    Task<IEnumerable<Competition>> GetAllAsync();
+}
---
+++ GoalData.Infrastructure/Data/Repositories/CompetitionRepository.cs
@@ -0,0 +1,24 @@
+﻿using GoalData.Application.Interfaces;
+using GoalData.Domain.Models;
+
+namespace GoalData.Infrastructure.Data.Repositories;
+
+public class CompetitionRepository : ICompetitionRepository
+{
+    private readonly string _connectionString;
+
+    public CompetitionRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public Task<IEnumerable<Competition>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Competition> GetByCodeAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
+}

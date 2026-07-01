7e492161a67b032d280f4237891ef5e442ebd58f
fabiosoares-silva <fabiosores.soares@gmail.com>
2026-07-01 20:04:04 -0300

    feat: implement GetAllAsync with ADO.NET and validate SQL Server connection

--- GoalData.Infrastructure/Data/Repositories/CompetitionRepository.cs
+++ GoalData.Infrastructure/Data/Repositories/CompetitionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using GoalData.Application.Interfaces;
 using GoalData.Domain.Models;
+using Microsoft.Data.SqlClient;
 
 namespace GoalData.Infrastructure.Data.Repositories;
 
@@ -12,9 +13,30 @@ public class CompetitionRepository : ICompetitionRepository
         _connectionString = connectionString;
     }
 
-    public Task<IEnumerable<Competition>> GetAllAsync()
+    public async Task<IEnumerable<Competition>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var competitions = new List<Competition>();
+
+        using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        var command = new SqlCommand("SELECT Id, Name, Code, Type, Emblem, LastUpdated FROM Competitions", connection);
+        var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            competitions.Add(new Competition
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Code = reader.GetString(2),
+                Type = reader.GetString(3),
+                Emblem = reader.GetString(4),
+                LastUpdate = reader.GetString(5)
+            });
+        }
+
+        return competitions;
     }
 
     public Task<Competition> GetByCodeAsync(string code)
